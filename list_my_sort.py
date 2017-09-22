89cb43a7b8e69332c0aa561620f47395202dfd3a
Sasha1152 <saxin@ukr.net>
2017-09-22 17:52:03 +0300

    sort

---
+++ list_my_sort.py
@@ -0,0 +1,12 @@
+a = [3, 2, 1, 0]
+
+for i in range(1, len(a)):
+    key = a[i]
+    while i:
+        if key < a[i - 1]:
+            a[i] = a[i - 1]
+            print a
+            a[i - 1] = key
+            print a
+        i -= 1
+print a
