c35041464ee78ba489ccfb776b89f5e8fc9b70e9
Sasha1152 <saxin@ukr.net>
2017-09-23 23:49:49 +0300

    sort

--- 01.py
+++ 01.py
@@ -1,11 +1,7 @@
-a = [3, 2, 1, 0]
+n = 5
+print 'n =', n
+while n != 0:
+   print n
+   n -= 1
 
-for i in range(1, len(a)):
-   j = i - 1
-   key = a[i]
-   while (a[j] > key) and (j >= 0):
-      a[j+1] = a[j]
-      j -= 1
-   a[j+1] = key
-
-print a
+print 'n =', n
--- list_insertion_sort.py
+++ list_insertion_sort.py
@@ -1,25 +1,35 @@
-lst = [3, 2, 1, 0]
-
-for i in xrange(1, len(lst)):
-    print 'before:', lst
-    j = i - 1
-    key = lst[i]
-    while (lst[j] > key) and (j >= 0):
-        lst[j + 1] = lst[j]
-        j -= 1
-        print lst
-    lst[j + 1] = key
-
-print lst
-
-print '---' * 8
-
 a = [3, 2, 1, 0]
 
+for i in xrange(1, len(a)):
+    j = i - 1
+    key = a[i]
+    while (a[j] > key) and (j >= 0):
+        a[j + 1] = a[j]
+        j -= 1
+    a[j + 1] = key
+
+print a
+
+print '---' * 5
+
+a = [3, 2, 1, 0]
 for i in range(1, len(a)):
     key = a[i]
     n = 1
     while key < a[i - n] and (i - n) >= 0:
-        a[i] = a[i - n]
+        a[i - n + 1] = a[i - n]
         n += 1
+    a[i - n + 1] = key
+print a
+
+print '---' * 5
+
+a = ['c', 'b', 'a']
+for i in range(1, len(a)):
+    key = a[i]
+    n = i
+    while n > 0 and key < a[n - 1]:
+        a[n] = a[n - 1]
+        n -= 1
+    a[n] = key
 print a
