34c46290e667a138b466cbd9a076f8b51b022621
Sasha1152 <saxin@ukr.net>
2017-09-28 23:40:51 +0300

    equation

--- equation.py
+++ equation.py
@@ -1,36 +1,59 @@
-s_start = raw_input('enter equation: ')
-s = []
-for i in s_start:
-    s.append(i)
+while True:
+    print 'enter equation(type "exit" to quit):'
+    s_start = raw_input()
+    if s_start == 'exit':
+        break
 
-s = s_start.split()
-# s = '10 + x = 30'.split()
-print s
+    s = s_start.split()
 
-for i in range(0, 5, 2):
-    try:
-        s[i] = int(s[i])
-        if i < 3:
-            known = s[i]
+    for i in range(0, 5, 2):
+        try:
+            s[i] = int(s[i])
+            if i < 3:
+                known = s[i]
+            else:
+                result = s[i]
+        except ValueError:
+            if i == 0 or 2:
+                unknown = s[i]
+            else:
+                continue
+
+    if s[1] == '+':
+        x = result - known
+        antisign = '-'
+
+    elif s[1] == '*':
+        x = result / known
+        antisign = '/'
+
+    elif s[1] == '-':
+        if s[0] == unknown:
+            x = result + known
+            antisign = '+'
+        elif s[2] == unknown:
+            x = known - result
+            antisign = '-'
+
+    elif s[1] == '/':
+        if s[0] == unknown:
+            x = result * known
+            antisign = '*'
+        elif s[2] == unknown:
+            x = known / result
+            antisign = '/'
+
+    for i in range(5):
+        if s[i] == unknown:
+            s[i] = str(x)
         else:
-            result = s[i]
-    except ValueError:
-        if i == 0 or 2:
-            unknown = s[i]
-        else:
-            continue
+            s[i] = str(s[i])
 
-if s[1] == '+':
-    x = result - known
-
-for i in range(5):
-    if s[i] == unknown:
-        s[i] = str(x)
+    if (s[1] == '-' or '/') and s[2] == unknown:
+        print '{} = {} {} {}'.format(unknown, known, antisign, result)
     else:
-        s[i] = str(s[i])
+        print '{} = {} {} {}'.format(unknown, result, antisign, known)
 
-# print s
-print '{} = {} - {}'.format(unknown, result, known)
-print '{} = {}'.format(unknown, x)
-print '-'*12
-print ' '.join(s)
+    print '{} = {}'.format(unknown, x)
+    print '-'*12
+    print ' '.join(s)
