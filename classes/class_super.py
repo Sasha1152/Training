46d51a3e8bffb3da6962ee5efc7affd08614d48d
Sasha1152 <saxin@ukr.net>
2017-10-18 23:41:06 +0300

    homework

---
+++ classes/class_super.py
@@ -0,0 +1,15 @@
+class A(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class B(A):
+    def __init__(self, name, age):
+        super(A).__init__(name)
+        self.age = age
+
+one = A('Sasha')
+print one.name
+
+two = B('Kate', '12')
+print two.age
