68ce8021ed92075f7d708366e23e156b26987355
Sasha1152 <saxin@ukr.net>
2017-10-20 18:04:32 +0300

    work

--- 01.py
+++ 01.py
@@ -1,4 +1,12 @@
-import random
+n = int(input())
+m = 1
+while m < n:
+    for i in range(n - m):
+        print '.',
+    for i in range(m):
+        print '*',
+    m += 1
+    print
 
-for i in range(10):
-	print random.random()
+for i in range(n):
+    print '*',
