74bc3deff8858542b8a1924b217fb50f2a45cc34
Sasha1152 <saxin@ukr.net>
2017-10-24 18:06:10 +0300

    classes

---
+++ classes/class_polymorphism.py
@@ -0,0 +1,23 @@
+class Based:
+    def __init__(self, n):
+        self.n = n
+    def out(self ):
+        print self.n
+
+class One(Based):
+    def multi(self, m):
+        self.n *= m
+
+class Two(Based):
+    def inlist(self):
+        self.inlist = list(str(self.n))
+    def out(self):
+        print(self.inlist)
+
+
+obj1 = One(5)
+obj2 = Two('abc')
+obj1.multi(10)
+obj1.out()
+obj2.inlist()
+obj2.out()
