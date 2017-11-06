1c39ad096809bc64f5f3d804b9e3b06c267e4674
Sasha1152 <saxin@ukr.net>
2017-11-06 18:22:50 +0200

    work

--- classes/task-2.py
+++ classes/task-2.py
@@ -1,8 +1,27 @@
 """ the task see the book 'Simple Python' - Bill Lubanovich p.178 """
 
-class Bear():
-    def eats(self):
-        return 'berries'
+class Laser():
+    def does(self):
+        return 'disintegrate'
 
-bear = Bear()
-print(bear.eats())
+class Claw():
+    def does(self):
+        return 'crush'
+
+class SmartPhone():
+    def does(self):
+        return 'ring'
+
+class Robot():
+    obj_laser = Laser()
+    obj_claw = Claw()
+    obj_smart = SmartPhone()
+
+    @classmethod
+    def does(cls):
+        return cls.obj_laser.does(),\
+               cls.obj_claw.does(),\
+               cls.obj_smart.does()
+
+
+print(*Robot.does())
