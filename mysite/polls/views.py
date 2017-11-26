2cfb5f59c13fbd0b152e37d3ed46971520d8411f
Sasha1152 <saxin@ukr.net>
2017-11-26 23:51:29 +0200

    work

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,14 +5,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello, World')
+    text = 'Unfortunately, this Steve Jobs figurine may' \
+           'not ship in time for the holidays'
+    return HttpResponse(text)
 
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 
-def result(request, question_id):
+def results(request, question_id):
     response = "You're looking at the result of question %s"
     return HttpResponse(response % question_id)
 
