8969af6d9b7e406e6dfd8b90a1d4e449a332e7eb
Sasha1152 <saxin@ukr.net>
2017-11-27 18:05:21 +0200

    templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Question
 
 # Create your views here.
 
 
 def index(request):
-    text = 'Unfortunately, this Steve Jobs figurine may' \
-           'not ship in time for the holidays'
-    return HttpResponse(text)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ' ||| '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
