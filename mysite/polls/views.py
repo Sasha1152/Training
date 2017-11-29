32bf0131a96f908998055bc79787bc764a996fd7
Sasha1152 <saxin@ukr.net>
2017-11-29 18:23:52 +0200

    work

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.http import Http404
+# from django.template import loader
 from . models import Question
 
 # Create your views here.
@@ -13,17 +14,25 @@ from . models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {'latest_question_list': latest_question_list})
-    return HttpResponse(template.render(context))
+    # template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Sorry. Question does not exist')
+    # return HttpResponse("You're looking at question %s." % question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     response = "You're looking at the result of question %s"
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s" % question_id)
