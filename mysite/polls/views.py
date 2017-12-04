97789f2cefc7c6254a7853d479a3ed58098a8f9c
Sasha1152 <saxin@ukr.net>
2017-12-04 17:54:34 +0200

    work

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -25,7 +25,7 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
-    def queryset(self):
+    def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
@@ -57,6 +57,7 @@ def results(request, question_id):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
