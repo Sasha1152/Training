ebcdae60f430f3a0462c02adae02e3794a7e8cbb
Sasha1152 <saxin@ukr.net>
2017-12-13 18:23:15 +0200

    created new model 'moons'

--- mysite/planets/admin.py
+++ mysite/planets/admin.py
@@ -3,7 +3,11 @@ from . models import Planets
 
 
 class PlanetsAdmin(admin.ModelAdmin):
-    fields = ['planet_number', 'planet_name', 'planet_mass']
+    fields = ['planet_number', 'planet_name']
+    list_display = ('planet_number',
+                    'planet_name',
+                    'planet_mass_to_earth',
+                    'satellites',)
 
 
 admin.site.register(Planets)
