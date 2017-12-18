01bed62bbddd11bb4342e7fc40744ed96de556a1
Sasha1152 <saxin@ukr.net>
2017-12-18 17:19:36 +0200

    work

--- kwargs.py
+++ kwargs.py
@@ -11,14 +11,14 @@ func2(a=1, b=2, c=3)  # {'a': 1, 'c': 3, 'b': 2} - dict
 
 
 def func3(a, b, c):
-    print a, b, c
+    print(a, b, c)
 
 func3(**{'a': 1, 'c': 3, 'b': 2})  # 1 2 3
 
 
 def func4(**kwargs):
     for i in kwargs:
-        print i
+        print(i)
 
 kwargs = {'a': 1, 'b': 2, 'c': 3}
 func4(**kwargs)
