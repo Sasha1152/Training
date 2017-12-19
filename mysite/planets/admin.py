f84920046849b1f78d0a995a7c7782014069fe78
Sasha1152 <saxin@ukr.net>
2017-12-19 17:35:56 +0200

    created abstract model

--- mysite/planets/admin.py
+++ mysite/planets/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-from . models import Planet
+from . models import Planet, Moon
+
+
+class MoonInLine(admin.TabularInline):
+    model = Moon
+    list_display = ('planet_mother')
 
 
 class PlanetAdmin(admin.ModelAdmin):
     # fields = ['planet_number', 'planet_name']
-    list_display = ('planet_number',
-                    'planet_name',
+    list_display = ('number',
+                    'name',
                     'planet_mass_to_earth',
+                    'orbital_speed',
                     'moons_quantity',
-                    'planet_image',)
-
+                    'image',)
+    inlines = [MoonInLine]
 
 admin.site.register(Planet, PlanetAdmin)
--- mysite/planets/models.py
+++ mysite/planets/models.py
@@ -1,33 +1,40 @@
 from django.db import models
+import datetime
 
 
-class Planet(models.Model):
-    planet_number = models.PositiveSmallIntegerField(primary_key=True)
-    planet_name = models.CharField(max_length=30)
-    planet_mass_to_earth = models.DecimalField(blank=True, max_digits=10, decimal_places=4, verbose_name='planet mass relative to an Earth')
-    moons_quantity = models.PositiveSmallIntegerField(blank=True, default=0)
-    planet_image = models.ImageField(blank=True)
-    planet_description = models.TextField(blank=True)
+class CommonData(models.Model):
+    name = models.CharField(max_length=30)
+    number = models.PositiveSmallIntegerField(primary_key=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+    discovery_date = models.DateField(null=True, blank=True)
+    description = models.TextField(blank=True)
+    wiki = models.URLField(blank=True)
 
     class Meta:
-        ordering = ['planet_number']
+        abstract = True
+        ordering = ['number']
+
+
+class Planet(CommonData):
+    planet_mass_to_earth = models.DecimalField(blank=True, max_digits=10, decimal_places=4, verbose_name='planet mass relative to an Earth')
+    orbital_speed = models.DecimalField(blank=True, max_digits=5, decimal_places=3, verbose_name='Average orbital speed, km/s')
+    moons_quantity = models.PositiveSmallIntegerField(blank=True, default=0)
+
+    class Meta:
         verbose_name = 'planet'
         verbose_name_plural = 'planets'
 
     def __str__(self):
-        return self.planet_name
+        return self.name
 
 
-class Moon(models.Model):
+class Moon(CommonData):
     planet_mother = models.ForeignKey(Planet, on_delete=models.CASCADE)
-    moon_number = models.PositiveSmallIntegerField(primary_key=True)
-    moon_name = models.CharField(max_length=30)
 
     class Meta:
-        ordering = ['moon_number']
         verbose_name = 'moon'
         verbose_name_plural = 'moons'
 
     def __str__(self):
-        return self.planet_mother + '--' + self.moon_name
+        return str(self.planet_mother) + '--' + str(self.name)
 
