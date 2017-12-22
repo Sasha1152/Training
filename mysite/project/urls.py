472592a5e82f7c619c5d87e31faea50bbece4d84
Sasha1152 <saxin@ukr.net>
2017-12-23 00:11:07 +0200

    reated new app 'boards'

--- mysite/project/urls.py
+++ mysite/project/urls.py
@@ -17,10 +17,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'polls/', include('polls.urls'),)
+    url(r'polls/', include('polls.urls'),),
+    url(r'^$', include('boards.urls'),),
 ]
 
 # For static files
