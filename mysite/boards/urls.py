e3badfbf00484d0a9e00a414dff9dead5aad90bc
Sasha1152 <saxin@ukr.net>
2018-02-01 18:05:08 +0200

    work with 'urls'

--- mysite/boards/urls.py
+++ mysite/boards/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'(?P<pk>\d+)/$', views.board_topics, name='board_topics')
+    url(r'(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
