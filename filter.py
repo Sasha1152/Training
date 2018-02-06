98231b48cbf5f1299068ac25d07fb7ca1dbf3f68
Sasha1152 <saxin@ukr.net>
2018-02-06 17:06:57 +0200

    little fixing

--- filter.py
+++ filter.py
@@ -1,6 +1,6 @@
 my_list = [0, 5, 100, 4, 2, 11, 55, 10]
 
 print(list(filter(lambda x: x != 'a', my_list)))
-print(filter(lambda x: x == 'a', my_list))
-print(filter(lambda x: x % 2, my_list))
+print(list(filter(lambda x: x == 'a', my_list)))
+print(list(filter(lambda x: x % 2, my_list)))
 print(filter(lambda x: x % 5, my_list))
