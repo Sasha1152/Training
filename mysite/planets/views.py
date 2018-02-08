e600c2661b2e7b1380bd75a43f77dec7f99279cb
Sasha1152 <saxin@ukr.net>
2018-02-08 17:57:41 +0200

    work with 'urlconf'

--- mysite/planets/views.py
+++ mysite/planets/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . models import Planet
 
+
 def home(request):
-	# return HttpResponse('Planets. The home page')
-	planets = Planet.objects.all()
-	# return HttpResponse(planets)
-	return render(request, 'home.html', {'planets': planets})
+    # return HttpResponse('Planets. The home page')
+    planets = Planet.objects.all()
+    # return HttpResponse(planets)
+    return render(request, 'planets/home.html', {'planets': planets})
