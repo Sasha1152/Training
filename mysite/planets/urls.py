59eb158cae5fc28bb9735f5e2e235b2e121ea2cc
Sasha1152 <saxin@ukr.net>
2018-02-14 23:42:32 +0200

    14.02.2018 create homepage

--- mysite/planets/urls.py
+++ mysite/planets/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'planets'
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.planets_list, name='planets_list'),
     url(r'page_1/$', views.page_1),
     url(r'page_2/$', views.page_2)
 
--- mysite/planets/views.py
+++ mysite/planets/views.py
@@ -3,15 +3,15 @@ from django.http import HttpResponse
 from . models import Planet
 
 
-def home(request):
+def planets_list(request):
     # return HttpResponse('Planets. The home page')
     planets = Planet.objects.all()
     # return HttpResponse(planets)
-    return render(request, 'planets/home.html', {'planets': planets})
+    return render(request, 'planets_list.html', {'planets': planets})
 
 
 def page_1(request):
-    return render(request, 'planets/page_1.html')
+    return render(request, 'page_1.html')
 
 
 def page_2(request):
