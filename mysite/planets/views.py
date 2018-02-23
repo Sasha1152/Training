145f419e3990b41b324475a5a2c0e6d23cbbaf52
Sasha1152 <saxin@ukr.net>
2018-02-24 00:30:13 +0200

    templates & tags

--- mysite/planets/views.py
+++ mysite/planets/views.py
@@ -3,17 +3,17 @@ from django.http import HttpResponse
 from . models import Planet
 
 
-def planets_list(request):
+def planets_list(request, pk):
     # return HttpResponse('Planets. The home page')
     planets = Planet.objects.all()
     # return HttpResponse(planets)
-    return render(request, 'planets_list.html', {'planets': planets})
+    return render(request, 'planets_list.html', {'planets': planets, 'planet_number': pk})
 
 
 def planet_data(request, pk):
     planet = Planet.objects.get(pk=pk)
     # return HttpResponse(planet)
-    return render(request, 'planet_data.html', {'planet': planet})
+    return render(request, 'planet_data.html', {'planet': planet, 'pk': pk})
 
 
 def page_1(request):
