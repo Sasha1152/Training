b9954184bde9b4d93e97980fe44b3fcff8324703
Sasha1152 <saxin@ukr.net>
2018-03-05 23:33:15 +0200

    {% url 'planets:number' %}

--- mysite/planets/urls.py
+++ mysite/planets/urls.py
@@ -5,5 +5,5 @@ app_name = 'planets'
 
 urlpatterns = [
     url(r'^$', views.planets_list, name='planets_list'),
-    url(r'^(?P<pk>[0-9]+)/$', views.planet_data, name='planet_data'),
+    url(r'^(?P<number>[0-9]+)/$', views.planet_data, name='planet_data'),
 ]
