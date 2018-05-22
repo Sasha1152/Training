0f1151da493d2a5fc33a1a8dc8507578119dc4f3
Sasha1152 <saxin@ukr.net>
2018-05-22 17:13:41 +0300

    chart of planets radiuses

--- mysite/planets/static/chart_radius.js
+++ mysite/planets/static/chart_radius.js
@@ -4,25 +4,27 @@ google.charts.setOnLoadCallback(drawSeriesChart);
     function drawSeriesChart() {
 
       var data = google.visualization.arrayToDataTable([
-        ['ID', 'Life Expectancy', 'Fertility Rate', 'Region',     'Population'],
-        ['CAN',    80.66,              1.67,      'North America',  33739900],
-        ['DEU',    79.84,              1.36,      'Europe',         81902307],
-        ['DNK',    78.6,               1.84,      'Europe',         5523095],
-        ['EGY',    72.73,              2.78,      'Middle East',    79716203],
-        ['GBR',    80.05,              2,         'Europe',         61801570],
-        ['IRN',    72.49,              1.7,       'Middle East',    73137148],
-        ['IRQ',    68.09,              4.77,      'Middle East',    31090763],
-        ['ISR',    81.55,              2.96,      'Middle East',    7485600],
-        ['RUS',    68.6,               1.54,      'Europe',         141850000],
-        ['USA',    78.09,              2.05,      'North America',  307007000]
+        ['Name', 'Position', 'coordinate', 'Type',     'Radius, km'],
+        ['Mercury', 1,          3,      'solid surface',  2440],
+        ['Venus',   2,          3,      'solid surface',  6052],
+        ['Earth',   3,          3,      'solid surface',  6371],
+        ['Moon',    3,          2,      'solid surface',  1737],
+        ['Mars',    4,          3,      'solid surface',  3390],
+        ['Phobos',  4,          2,      'solid surface',  11],
+        ['Deimos',  4,          1,      'solid surface',  6],
+        ['Jupiter', 5,          3,      'gas surface',    69911],
+        ['Saturn',  6,          3,      'gas surface',    58232],
+        ['Uranus',  7,          3,      'gas surface',    25362],
+        ['Neptune', 8,          3,      'gas surface',    24622],
+        ['Pluto',   9,          3,      'solid surface',  1188],
       ]);
 
       var options = {
-        title: 'Correlation between life expectancy, fertility rate ' +
-               'and population of some world countries (2010)',
-        hAxis: {title: 'Life Expectancy'},
-        vAxis: {title: 'Fertility Rate'},
-        bubble: {textStyle: {fontSize: 11}}
+        title: 'Sizes of planets',
+        bubble: {textStyle: {fontSize: 11}},
+        chartArea: {left:30, top:30, bottom:30, right:100,
+                    width:'10%', height:'10%', backgroundColor: 'grey'},
+        backgroundColor: 'black',
       };
 
       var chart = new google.visualization.BubbleChart(document.getElementById('series_chart_div'));
