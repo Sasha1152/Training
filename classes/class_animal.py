0b3482461327fe5b34cb4da042168d83cac80de4
Sasha1152 <saxin@ukr.net>
2018-07-05 00:04:07 +0300

    prepearing to the test

--- classes/class_animal.py
+++ classes/class_animal.py
@@ -1,27 +1,12 @@
 class Animal:
     def __init__(self, name):
         self.name = name
-    def talk(self):
-        raise NotImplementedError('Subclass must implement abstract method')
 
 class Cat(Animal):
-    def talk(self):
-        return 'Meow'
+    def __init__(self, name):
+        self.name = name
 
-class Dog(Animal):
-    def talk(self):
-        return 'Woof, woof'
 
-animals = [Cat('Missy'), Cat('Jack'), Cat('Lassie')]
-for animal in animals:
-    print (animal.name + ': ' + animal.talk())
+a = Cat('as')
 
-print(Dog('Butcher'))
-print(Dog('Butcher').name)
-print(Dog('Butcher').talk())
-print(Dog('Butcher').talk)
-print(Cat('Missy').name)
-print(Cat)
-print('----')
-abstr = Animal('Zorro')
 print(abstr.talk())
