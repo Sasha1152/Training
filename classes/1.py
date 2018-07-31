219b5596f26a9473f3358affc549a5182af03f74
Sasha1152 <saxin@ukr.net>
2018-07-31 18:26:53 +0300

    classes

--- classes/1.py
+++ classes/1.py
@@ -1,18 +1,6 @@
-class A(object):
-    mind = 'yes'
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
+n  = 10
+def total(a):
+    total = n + a
+    return total
 
-
-class B(A):
-    def __init__(self, name, surname, age, gender):
-        super(B, self).__init__(name, surname)
-        self.age = 36
-        self.gender = gender
-
-obj_a = A('Kate', 'Rozd')
-obj_b = B('Sasha', 'Shmig', 36, 'male')
-
-print(obj_a.name, obj_a.surname)
-print(obj_b.name, obj_b.age)
+print(total(1))
--- classes/self.py
+++ classes/self.py
@@ -8,6 +8,8 @@ class A:
 husband = A('Sasha')
 
 print(husband.name)
+husband.name2 = 'Alex'
+print(husband.name2)
 print(husband.pri())
 # the same:
 print(A.pri(husband))
