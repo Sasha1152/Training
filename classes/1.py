c18a9b9446b7aa4d1495b91ba4c48d7d7ffbc19a
Sasha1152 <saxin@ukr.net>
2018-09-30 23:57:56 +0300

    work with HTML

--- classes/1.py
+++ classes/1.py
@@ -1,18 +1,3 @@
-class A(object):
-    mind = 'yes'
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-
-
-class B(A):
-    def __init__(self, name, surname, age, gender):
-        super(B, self).__init__(name, surname)
-        self.age = 36
-        self.gender = gender
-
-obj_a = A('Kate', 'Rozd')
-obj_b = B('Sasha', 'Shmig', 36, 'male')
-
-print(obj_a.name, obj_a.surname)
-print(obj_b.name, obj_b.age)
+a = {1:1}
+b =dict(a)
+print(b)
