0c8c585a0571e2b5e7d49bf2942ce35709174772
Sasha1152 <saxin@ukr.net>
2018-10-10 19:21:53 +0300

    sys.version_info

--- sys.py
+++ sys.py
@@ -3,3 +3,5 @@ import sys
 print('sys.path----->', sys.path)
 print('dir----->', dir(sys))
 print('info---->', sys.platform)
+print('Python version----->', sys.version)
+print('Python version----->', sys.version_info)
