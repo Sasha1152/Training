2eeb2d09a527710ff599c3ec6b715a4515f56fad
Sasha1152 <saxin@ukr.net>
2018-10-18 13:41:27 +0300

    lern super() function

---
+++ classes/super().py
@@ -0,0 +1,31 @@
+# https://www.journaldev.com/15911/python-super
+class A:
+    def __init__(self):
+        print('Initializing: class A')
+
+    def sub_method(self, n):
+        print('Printing from class A :', n)
+
+
+class B(A):
+    def __init__(self):
+        print('Initializing: class B')
+        super().__init__()
+
+    def sub_method(self, n):
+        print('Printing from class B :', n)
+        super().sub_method(n + 1)
+
+
+class C(B):
+    def __init__(self):
+        print('Initializing: class C')
+        super().__init__()
+
+    def sub_method(self, n):
+        print('Printing from class C :', n)
+        super().sub_method(n + 1)
+
+
+c = C()
+c.sub_method(1)
