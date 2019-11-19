785324ee61f4dce4e95b76e31047beba32b3e2a5
Sasha1152 <saxin@ukr.net>
2019-11-20 01:06:33 +0200

    parsing task

---
+++ work with files/parsing_plumbers.py
@@ -0,0 +1,56 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+r = requests.get('https://www.yellowpages.com/austin-tx/plumbers')
+soup = BeautifulSoup(r.text, 'html5lib')
+companies = soup.body.find('div', {'class': 'search-results organic'}).find_all('div', {'class': 'info'})
+
+
+def get_company_name(com):
+    if com.find('a', {'class': 'track-map-it directions'}):
+        return com.find('a', {'class': 'business-name'}).text
+    else:
+        return False
+
+
+def get_address(com):
+    try:
+        street = com.find('div', {'class': 'street-address'}).text
+    except AttributeError:
+        street = ''
+
+    try:
+        locality = com.find('div', {'class': 'locality'}).text
+    except AttributeError:
+        locality = ''
+
+    return street + locality
+
+
+def get_phone(com):
+    try:
+        phone = com.find('div', {'class': 'phones phone primary'}).text
+    except AttributeError:
+        return None
+    phone_pure = ''
+    for symbol in phone:
+        if symbol.isdigit():
+            phone_pure += symbol
+    return int(phone_pure)
+
+
+plumbers = {}
+for company in companies:
+    company_name = get_company_name(company)
+    if company_name:
+        plumbers[company_name] = {}
+        plumbers[company_name]['address'] = get_address(company)
+        plumbers[company_name]['phone_number'] = get_phone(company)
+
+plumbers_serialised = json.dumps(plumbers)
+
+with open('plumbing.json', 'w') as plumbers_data_file:
+    plumbers_data_file.write(plumbers_serialised)
+
+print('The file "plumbing.json" successfully created!')
