8e34d8ffbb0f089be3d0359f22ee0e5b422827a7
Sasha1152 <saxin@ukr.net>
2019-12-09 13:35:00 +0200

    plvisin test resolve

---
+++ test_task/plvision_test.py
@@ -0,0 +1,33 @@
+def is_good_string(string):
+    """
+        >>> is_good_string('()')
+        True
+        >>> is_good_string('()[]{}')
+        True
+        >>> is_good_string('(]')
+        False
+        >>> is_good_string('([)]')
+        False
+        >>> is_good_string('{[]}')
+        True
+    """
+
+    brackets = '([{}])'
+    opening_brackets = '([{'
+    brackets_dict = {'(': ')', '[': ']', '{': '}'}
+    opened_bracket = []
+
+    for i in string:
+        if i in brackets:
+            if i in opening_brackets:
+                opened_bracket.append(i)
+
+            elif i == brackets_dict.get(opened_bracket[-1]):
+                opened_bracket.pop()
+
+    return not len(opened_bracket)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
