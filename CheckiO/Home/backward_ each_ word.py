b159a2dd9034ad3a80fa7505f80071c8c9f86a77
Sasha1152 <saxingit config --global user.email saxin@ukr.netgit config --global user.email saxin>
2023-01-29 23:31:05 +0200

    checkio: backward each word

--- CheckiO/Home/backward_ each_ word.py
+++ CheckiO/Home/backward_ each_ word.py
@@ -1,14 +1,12 @@
-def backward_string_by_word(text: str) -> str:
-    rewersed_text = ''
-    rewersed_word = ''
-    for letter in text:
-        if letter:
-            rewersed_word = letter + rewersed_word
-        else:
-            rewersed_word = ''
-        rewersed_text += rewersed_word
-    return rewersed_text
+# https://py.checkio.org/uk/mission/backward-each-word/
 
+def backward_string_by_word(text: str) -> str:
+    text_list = text.split(' ')
+    for i in range(len(text_list)):
+        text_list[i] = text_list[i][::-1]
+    rewersed_text = ' '.join(text_list)
+
+    return rewersed_text
 
 
 print(backward_string_by_word(''))
@@ -16,3 +14,15 @@ print(backward_string_by_word('world'))
 print(backward_string_by_word('hello world'))
 print(backward_string_by_word('hello   world'))
 print(backward_string_by_word('welcome to a game'))
+
+
+def backward_string_by_word(text: str) -> str:
+    return " ".join(map(lambda x: x[::-1], text.split(' ')))
+
+
+def backward_string_by_word2(text: str) -> str:
+   return ' '.join(reversed(text[::-1].split(' ')))
+
+
+def backward_string_by_word3(text: str) -> str:
+    return ' '.join([word[::-1] for word in text.split(' ')])
