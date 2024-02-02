044382bf7e5a2196a655e0691241018b4f92bf83
Sasha1152 <saxin@ukr.net>
2024-02-02 16:45:52 +0200

    roman numerals

--- CheckiO/Ice Base/roman_numerals.py
+++ CheckiO/Ice Base/roman_numerals.py
@@ -1,20 +1,43 @@
 # https://py.checkio.org/ru/mission/roman-numerals/
 
-print('Hello. This program converts Arabic numerals in Rome.')
-print('*'*25)
-while True:
-    try:
-        a =int(input('enter a number from 1 to 10, press any key to exit: '))
-        length = len(str(a))
-    except SyntaxError:
-        break
-    except NameError:
-        break
-    if a >= 1 and a<=3: print('I' * a)
-    elif a == 4: print('IV')
-    elif a == 5: print('V')
-    elif a > 5 and a < 9: print('V'+'I'*(a - 5))
-    elif a == 9: print('IX')
-    elif a == 10: print('X')
-    print('Do you want to continue?')
-    
+def checkio(n):
+    """
+    I	1 (unus)
+    V	5 (quinque)
+    X	10 (decem)
+    L	50 (quinquaginta)
+    C	100 (centum)
+    D	500 (quingenti)
+    M	1000 (mille)
+    """
+    # rom_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    arab_numbers = {1000: 'M',
+                    900: 'CM',
+                    500: 'D',
+                    400: 'CD',
+                    100: 'C',
+                    90: 'XC',
+                    50: 'L',
+                    40: 'XL',
+                    10: 'X',
+                    9: 'IX',
+                    5: 'V',
+                    4: 'IV',
+                    1: 'I'}
+    rom_number = ''
+
+    for key in arab_numbers:
+        main, remainder = divmod(n, key)
+        if remainder == n:
+            continue
+        else:
+            rom_number += arab_numbers[key] * main
+            n = remainder
+
+    return rom_number
+
+
+print(checkio(9))
+print(checkio(76))  # 'LXXVI'
+print(checkio(499))  # 'CDXCIX'
+print(checkio(3888))  # 'MMMDCCCLXXXVIII'
